92573de1b3924891a44df5dcca577b2373f90946
lufheran <lufheran@gmail.com>
2015-09-11 16:39:26 -0500

    Creacion del template

--- js/index.js
+++ js/index.js
@@ -2,6 +2,13 @@
  * Created by Lufheran on 07/09/15.
  */
 (function() {
+
+    var API_WORLDTIME_KEY = "d6a4075ceb419113c64885d9086d5";
+    var API_WORLDTIME = "https://api.worldweatheronline.com/free/v2/tz.ashx?format=json&key=" + API_WORLDTIME_KEY + "&q=";
+    var API_WEATHER_KEY = "80114c7878f599621184a687fc500a12";
+    var API_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?APPID=" + API_WEATHER_KEY + "&";
+    var IMG_WEATHER = "http://openweathermap.org/img/w/";
+
     if(navigator.geolocation ){
         navigator.geolocation.getCurrentPosition(getCoords, errorFound);
 
@@ -9,11 +16,10 @@
         alert("Not supported for your browser")
     }
 
-    var API_WEATHER_KEY =""
-
     function errorFound(error){
-        alert("An mistake occurred"+ error.code);
+
         var message  = "An mistake occurred";
+
         switch (error.code){
             case 0:
                 alert(message +", unknow");
